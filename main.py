70713a3ff7ffcf2c2f23121019488a9099801b59
20338 <80858939+20338@users.noreply.github.com>
2021-07-28 22:50:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+""" This code is made by Nathan Brown 6/07/21 this is a te reo quiz made to help users by teaching them basic te reo maaori"""
+
+
+import time
+
+print ("Welcome to the te reo quiz what is youre name?")
+
+name = input()
+
+print ("do you know how to play " + name + "?")
+
+intrustions_help = input().lower()
